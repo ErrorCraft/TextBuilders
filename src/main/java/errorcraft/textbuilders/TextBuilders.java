332cb7e96278f1cca71823a1cc3009f65471c7bc
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-10-27 17:24:17 +0200

    Add project files

---
+++ src/main/java/errorcraft/textbuilders/TextBuilders.java
@@ -0,0 +1,9 @@
+package errorcraft.textbuilders;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TextBuilders implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
